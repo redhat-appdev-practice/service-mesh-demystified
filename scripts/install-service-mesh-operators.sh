1a88845a08e6b228a2ec3330adcb0226f4f78c42
mm7259 <mm7259@nyu.edu>
2021-03-18 16:46:52 -0400

    removed the duplicate service-mesh helm chart from root and modified the scripts in the scripts directory at the root

--- scripts/install-service-mesh-operators.sh
+++ scripts/install-service-mesh-operators.sh
@@ -2,7 +2,7 @@
 
 echo "Installing service mesh operators..."
 
-helm install service-mesh-operators -n openshift-operators service-mesh/service-mesh-operators
+helm install service-mesh-operators -n openshift-operators charts/install_sm_operators_with_helm/service-mesh-operators
 
 echo "If you chose manual approval, approve the InstallPlans within the openshift-operators namespace at the following URL..."
 echo
